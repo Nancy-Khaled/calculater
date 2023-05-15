31eb286bb18082defd745f1f5d2d9354466d7b96
Nancy-Khaled <131464596+Nancy-Khaled@users.noreply.github.com>
2023-05-15 21:13:10 +0300

    update Calculator

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser")
 
 const app = express()
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
@@ -15,6 +16,21 @@ app.post("/", function(req, res){
     var result = num1 + num2;
 
     res.send("The result of calculation is " + result)
+})
+
+//////////////////////////////////////////
+
+app.get('/bmicalculator', function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function(req, res){
+    
+    var w = Number(req.body.w);
+    var h = Number(req.body.h);
+    var bmiResult = Math.round(w / Math.pow(h, 2));
+
+    res.send("Your BMI is " + bmiResult)
 })
 
 app.listen(3000, function() {
